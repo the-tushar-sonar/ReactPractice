4b415e415e92ed1233138a9c2b786d6986a6d6a5
the-tushar-sonar <mylordtushar@gmail.com>
2025-06-26 20:52:43 +0530

    Primary layout is created

--- 02_BackgroundChanger/src/App.jsx
+++ 02_BackgroundChanger/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("olive")
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white  px-3 py-2 rounded-3xl'>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg' style={{ backgroundColor: "red" }}>Red</button>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg' style={{ backgroundColor: "green" }}>Green</button>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg' style={{ backgroundColor: "blue" }}>Blue</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
