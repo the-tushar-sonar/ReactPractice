81aabfd73b085d7c2c4d3c292b10ef5e5ef7b792
the-tushar-sonar <mylordtushar@gmail.com>
2025-06-27 19:12:47 +0530

    feat: initialize Currency Converter project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies for React, Vite, and Tailwind CSS.
    - Created public assets including Vite logo SVG.
    - Set up basic styling with App.css and index.css using Tailwind CSS.
    - Implemented main application structure in App.jsx and main.jsx.
    - Configured Vite with react and tailwindcss plugins in vite.config.js.
    - Added React logo SVG asset for branding.

---
+++ 04_CurrencyConvertor/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold bg-orange-500 text-center p-2">Currency Converter | Tushar</h1>
+    </>
+  )
+}
+
+export default App
