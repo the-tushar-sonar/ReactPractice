63a4ca806a0aa0eeefe2113084d82a6e3889dbd3
the-tushar-sonar <mylordtushar@gmail.com>
2025-06-27 20:55:16 +0530

    feat: implement currency conversion functionality with InputBox component and custom hook

--- 04_CurrencyConvertor/src/App.jsx
+++ 04_CurrencyConvertor/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+
+  const options = Object.keys(currencyInfo);
 
   return (
     <>
-      <h1 className="text-3xl font-bold bg-orange-500 text-center p-2">Currency Converter | Tushar</h1>
+      <h1 className="text-3xl font-bold bg-orange-500 text-center p-2">
+        Currency Converter | Tushar
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
