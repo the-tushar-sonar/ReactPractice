57c88abeb8567115455f613254284ad0749eec83
the-tushar-sonar <mylordtushar@gmail.com>
2025-06-30 21:37:57 +0530

    feat: add Login and Profile components, integrate UserContext for user authentication

--- 06_MiniContext/src/App.jsx
+++ 06_MiniContext/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <UserContextProvider>
       <h1>Let's Learn Context Api</h1>
-    </>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
   )
 }
 
---
+++ 06_MiniContext/src/components/Login.jsx
@@ -0,0 +1,23 @@
+import React,{useState,useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+const Login = () => {
+    const {username,setUsername} = useState('')
+    const {password,setPassword} = useState('')
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setUsername(username,password)
+    }
+  return (
+    <div>
+      <h2>Login</h2>
+      <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)} placeholder='username'/>
+    <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='password'/>
+    <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+}
+
+export default Login
