1079dd950be5f99f8a334333c5cfc7ad970790ba
the-tushar-sonar <mylordtushar@gmail.com>
2025-07-01 20:31:16 +0530

    refactor: clean up imports and improve Login component functionality

--- 06_MiniContext/src/App.jsx
+++ 06_MiniContext/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import UserContextProvider from './context/UserContextProvider'
 import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
 
--- 06_MiniContext/src/context/UserContextProvider.jsx
+++ 06_MiniContext/src/context/UserContextProvider.jsx
@@ -1,4 +1,4 @@
-import React, { Children } from "react"
+import React from "react"
 import UserContext from "./UserContext"
 
 const UserContextProvider=({children})=>{
