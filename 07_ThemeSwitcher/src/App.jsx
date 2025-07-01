f2cbfd132cc36dc9a7be908f6b1cbd8bcb3061b2
the-tushar-sonar <mylordtushar@gmail.com>
2025-07-01 21:21:21 +0530

    feat: implement ThemeContext and ThemeProvider for theme management

--- 07_ThemeSwitcher/src/App.jsx
+++ 07_ThemeSwitcher/src/App.jsx
@@ -1,20 +1,36 @@
 import { useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./contexts/theme";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* Theme Switcher Button */}
-        </div>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* Theme Switcher Button */}
+          </div>
 
-        <div className="w-full max-w-sm mx-auto">
-          {/* Cards */}
+          <div className="w-full max-w-sm mx-auto">{/* Cards */}</div>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
