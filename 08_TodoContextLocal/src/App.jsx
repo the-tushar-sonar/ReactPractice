5fd4a93bb83c65f5c00917d721e5088dc6256c6a
the-tushar-sonar <mylordtushar@gmail.com>
2025-07-03 13:53:34 +0530

    feat: add localStorage integration for todos management in App component

--- 08_TodoContextLocal/src/App.jsx
+++ 08_TodoContextLocal/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { TodoProvider } from "./contexts";
 
@@ -28,6 +28,18 @@ function App() {
       )
     );
   };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <TodoProvider
