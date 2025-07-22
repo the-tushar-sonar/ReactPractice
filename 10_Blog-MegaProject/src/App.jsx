43baccdff056382fc61de4792d6857ce54d8b0c5
the-tushar-sonar <mylordtushar@gmail.com>
2025-07-22 15:41:21 +0530

    fix: update import path for configuration file in AuthService

--- 10_Blog-MegaProject/src/App.jsx
+++ 10_Blog-MegaProject/src/App.jsx
@@ -3,6 +3,8 @@ import { useDispatch } from "react-redux";
 import "./App.css";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
+import {Footer,Header} from "./components"
+import {Outlet} from 'react-router-dom'
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -24,7 +26,7 @@ function App() {
   }, []);
 
   return !loading ? (
-    <div className="min-h-screen flex flex-wrap items-center content-between bg-gray-400">
+    <div className="min-h-screen flex flex-wrap items-center content-between bg-gray-400 text-center">
       <div className="w-full block">
         <Header/>
         <main>
