4047eb50f47f9d1f30a86e2f9fac9bacc3436748
the-tushar-sonar <mylordtushar@gmail.com>
2025-07-25 12:46:32 +0530

    feat: implement SignUp component for user registration

---
+++ 10_Blog-MegaProject/src/components/SignUp.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import authService from "../appwrite/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../store/authSlice";
+import { Button, Input, Logo } from "./index";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+
+function SignUp() {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+
+  const create = async (data) => {
+    setError("");
+    try {
+      const userData = await authService.createAccount(data);
+      if (userData) {
+        const userData = await authService.getCurrentUser();
+        if (userData) dispatch(login(userData));
+        navigate("/");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <Logo width="100%" />
+          </span>
+        </div>
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          Create a new account
+        </h2>
+        <p className="mt-2 text-center text-base text-black/60">
+          Already have an account?&nbsp;
+          <Link
+            to="/login"
+            className="font-medium text-primary transition-all duration-200 hover:underline"
+          >
+            Sign in
+          </Link>
+        </p>
+        {error && <p className="text-red-600 text-center mt-8">{error}</p>}
+        <form onSubmit={handleSubmit(create)} className="mt-8 space-y-5">
+          <Input
+            label="Name: "
+            type="text"
+            placeholder="Enter your name"
+            {...register("name", {
+              required: true,
+            })}
+          />
+          <Input
+            label="Email"
+            type="email"
+            placeholder="Enter your email"
+            {...register("email", {
+              required: true,
+              validate: {
+                matchPattern: (value) => {
+                  /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(value) ||
+                    "Email address must be a valid address";
+                },
+              },
+            })}
+          />
+          <Input
+            label="Password"
+            type="password"
+            placeholder="Enter your password"
+            {...register("password", {
+              required: true,
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters long",
+              },
+            })}
+          />
+          <Button type="submit" className="w-full">
+            Create Account
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
