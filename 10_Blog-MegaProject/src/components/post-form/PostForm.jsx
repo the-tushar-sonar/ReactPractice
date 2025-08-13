a5e3f37158c4b5ee56c46dea11bc83b0774a4ef3
the-tushar-sonar <mylordtushar@gmail.com>
2025-08-13 21:10:09 +0530

    feat: implement slug auto-generation & design PostForm layout

--- 10_Blog-MegaProject/src/components/post-form/PostForm.jsx
+++ 10_Blog-MegaProject/src/components/post-form/PostForm.jsx
@@ -1,5 +1,5 @@
-import React, { useActionState, useCallback } from "react";
-import { useForm } from "react-hook-form";
+import React, { useActionState, useCallback, useEffect } from "react";
+import { get, set, useForm } from "react-hook-form";
 import { Button, Input, Select, RTE } from "../index";
 import appwriteService from "../../appwrite/config";
 import { useNavigate } from "react-router-dom";
@@ -65,7 +65,81 @@ function PostForm({ post }) {
     return "";
   }, []);
 
-  return <div></div>;
+  useEffect(() => {
+    const subscription = watch((value, { name }) => {
+      if (name === "title") {
+        setValue("slug", slugTransform(value.title, { shouldValidate: true }));
+      }
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [watch, slugTransform, setValue]);
+
+  return (
+    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+      <div className="w-2/3 px-2">
+        <Input
+          label="Title :"
+          placeholder="Enter post title"
+          className="mb-4"
+          {...register("title", {
+            required: true,
+          })}
+        />
+        <Input
+          label="Slug"
+          placeholder="Enter post slug"
+          className="mb-4"
+          {...register("slug", {
+            required: true,
+          })}
+          onInput={(e) => {
+            setValue("slug", slugTransform(e.target.value), {
+              shouldValidate: true,
+            });
+          }}
+        />
+        <RTE
+          label="Content"
+          control={control}
+          defaultValue={getValues("content")}
+        />
+      </div>
+      <div className="w-1/3 px-2">
+        <Input
+          type="file"
+          label="Featured Image"
+          className="mb-4"
+          accept="image/png, image/jpeg, image/jpg, image/gif"
+          {...register("image", {
+            required: !post,
+          })}
+        />
+        {post && (
+          <div className="mb-4">
+            <img
+              src={appwriteService.getFilePreview(post.featuredImage)}
+              alt={post.title}
+              className="rounded-lg "
+            />
+          </div>
+        )}
+        <Select
+          options={["active", "inactive"]}
+          label="Status"
+          className="mb-4"
+          {...register("status", {
+            required: true,
+          })}
+        />
+        <Button type="submit" className="w-full">
+          {post ? "Update" : "Submit"}
+        </Button>
+      </div>
+    </form>
+  );
 }
 
 export default PostForm;
