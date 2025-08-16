9f6f70cd2b0043d003cf289d3d83dce9bc315fc9
Tushar Sonar <107312969+the-tushar-sonar@users.noreply.github.com>
2025-08-16 14:11:22 +0530

    Update RTE.jsx

--- 10_Blog-MegaProject/src/components/RTE.jsx
+++ 10_Blog-MegaProject/src/components/RTE.jsx
@@ -11,7 +11,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey='wfm09eyebljcfy46cp63v2226tc5k3r9pk5zpxf6spwayavf'
+            apiKey='xyz'
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
